661649428daca0eae96b8aa043e52b3b7aedc5ae
OhNishant <nishant.mishra550@gmail.com>
2024-03-03 14:32:14 +0530

    stuf

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Lustria } from 'next/font/google';
+import { Lusitana, Inter, Lustria } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lustria = Lustria({ weight: '400', subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
